c41c127641399ee3dab032cd9c12fb626d66cba6
Unknown <ayoshhhh.hadola@gmail.com>
2020-02-18 12:08:30 +0100

    eerste stap

---
+++ function.php
@@ -0,0 +1,44 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "php-todo";
+
+// connectie maken
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=".$database, $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+// functie's om query uit te voeren
+
+function GetAllCheck($conn){
+    $query = $conn->prepare('SELECT * FROM todo');
+    $query->execute();
+    $result = $query->fetchAll();
+    return $result;
+}
+
+function GetCheck($id,$conn){
+    $query = $conn->prepare('SELECT * FROM todo WHERE id=:id');
+    $query->execute([':id' => $id]);
+    $result = $query->fetch();
+    return $result;
+}
+function DeleteCheck($id,$conn){
+    $query = $con->prepare('DELETE * FROM todo Where id=:id');
+    $query->execute([':id'=>$id]);
+    $result = $query->fetch();
+    return $result;
+}
+?>
+</body>
+</html>

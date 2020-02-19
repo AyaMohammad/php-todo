7309811d99ab761cf4b290ccb6e9d6330cfce38c
Unknown <ayoshhhh.hadola@gmail.com>
2020-02-19 11:54:45 +0100

    lijst toegevoegd

---
+++ list_create.php
@@ -0,0 +1,16 @@
+
+<?php
+    include 'header.php';
+?>
+
+<h2>HTML Forms</h2>
+
+<form action="/action_page.php">
+  <label for="name">list name:</label><br>
+  <input type="text" id="name" name="name" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+
